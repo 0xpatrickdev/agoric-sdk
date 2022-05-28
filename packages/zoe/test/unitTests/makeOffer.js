f1245f02e91d1b6985f5b341899cc62cbfa15804
Mark S. Miller <erights@gmail.com>
2022-05-27 20:31:28 -0700

    fix: schema limit minting and offers

--- packages/zoe/test/unitTests/makeOffer.js
+++ packages/zoe/test/unitTests/makeOffer.js
@@ -8,14 +8,26 @@ import { assert } from '@agoric/assert';
  * @param {ZCF} zcf
  * @param {Proposal=} proposal
  * @param {PaymentPKeywordRecord=} payments
+ * @param {Pattern} [proposalSchema]
  * @returns {Promise<{zcfSeat: ZCFSeat, userSeat: UserSeat}>}
  */
-export const makeOffer = async (zoe, zcf, proposal, payments) => {
+export const makeOffer = async (
+  zoe,
+  zcf,
+  proposal,
+  payments,
+  proposalSchema = undefined,
+) => {
   let zcfSeat;
   const getSeat = seat => {
     zcfSeat = seat;
   };
-  const invitation = await zcf.makeInvitation(getSeat, 'seat');
+  const invitation = await zcf.makeInvitation(
+    getSeat,
+    'seat',
+    undefined,
+    proposalSchema,
+  );
   const userSeat = await E(zoe).offer(invitation, proposal, payments);
   assert(zcfSeat);
   return { zcfSeat, userSeat };
