a1adfb6d69a34c0946129be13a6b8fb6c8c4c000
Turadg Aleahmad <turadg@agoric.com>
2022-05-31 09:26:23 -0700

    chore: fix tests

--- packages/zoe/test/unitTests/contracts/test-automaticRefund.js
+++ packages/zoe/test/unitTests/contracts/test-automaticRefund.js
@@ -1,4 +1,4 @@
-// @ts-check
+test / unitTests / contracts / test - automaticRefund.js;
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { test } from '@agoric/zoe/tools/prepare-test-env-ava.js';
 
