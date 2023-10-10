07ca03d23f0c9d9825c765a59743d9ac884a9dba
0xPatrick <patrick@0xpatrick.dev>
2023-10-10 12:38:00 -0400

    feat(builders): add-stATOM inter collateral proposal

---
+++ packages/builders/scripts/inter-protocol/add-stATOM.js
@@ -0,0 +1,49 @@
+import { makeHelpers } from '@agoric/deploy-script-support';
+import { defaultProposalBuilder as vaultProposalBuilder } from './add-collateral-core.js';
+import { defaultProposalBuilder as oraclesProposalBuilder } from './price-feed-core.js';
+
+/** @type {import('@agoric/deploy-script-support/src/externalTypes.js').ProposalBuilder} */
+export const starsVaultProposalBuilder = async powers => {
+  return vaultProposalBuilder(powers, {
+    interchainAssetOptions: {
+      denom:
+        'ibc/B60D2EF81DE7CCAE53C59B7850667595A7D580200C5636C8922ED78C95532BA7',
+      decimalPlaces: 6,
+      initialPrice: undefined,
+      issuerBoardId: undefined,
+      issuerName: 'stATOM',
+      keyword: 'STATOM',
+      oracleBrand: 'stATOM',
+      proposedName: 'stATOM',
+    },
+  });
+};
+
+/** @type {import('@agoric/deploy-script-support/src/externalTypes.js').ProposalBuilder} */
+export const starsOraclesProposalBuilder = async powers => {
+  return oraclesProposalBuilder(powers, {
+    AGORIC_INSTANCE_NAME: `stATOM-USD price feed`,
+    IN_BRAND_LOOKUP: ['agoricNames', 'oracleBrand', 'stATOM'],
+    IN_BRAND_DECIMALS: 6,
+    OUT_BRAND_LOOKUP: ['agoricNames', 'oracleBrand', 'USD'],
+    OUT_BRAND_DECIMALS: 4,
+    oracleAddresses: [
+      'agoric1ldmtatp24qlllgxmrsjzcpe20fvlkp448zcuce',
+      'agoric140dmkrz2e42ergjj7gyvejhzmjzurvqeq82ang',
+      'agoric1w8wktaur4zf8qmmtn3n7x3r0jhsjkjntcm3u6h',
+      'agoric10vjkvkmpp9e356xeh6qqlhrny2htyzp8hf88fk',
+      'agoric1qj07c7vfk3knqdral0sej7fa6eavkdn8vd8etf',
+      'agoric1lw4e4aas9q84tq0q92j85rwjjjapf8dmnllnft',
+      'agoric1ra0g6crtsy6r3qnpu7ruvm7qd4wjnznyzg5nu4',
+      'agoric1zj6vrrrjq4gsyr9lw7dplv4vyejg3p8j2urm82',
+      'agoric15xddzse9lq74cyt6ev9d7wywxerenxdgxsdc3m',
+      'agoric1w5wmck6q2xrt20ax3njlk2k87m4t4l2y2xgw2d',
+    ],
+  });
+};
+
+export default async (homeP, endowments) => {
+  const { writeCoreProposal } = await makeHelpers(homeP, endowments);
+  await writeCoreProposal('add-stATOM', starsVaultProposalBuilder);
+  await writeCoreProposal('add-stATOM-oracles', starsOraclesProposalBuilder);
+};
